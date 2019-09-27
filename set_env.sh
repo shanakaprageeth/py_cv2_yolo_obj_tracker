79a62c6cec2f76a81047552e4a45c4d6cdca8dbf
shanakaprageeth <shanakaprageeth@gmail.com>
2019-09-27 19:19:01 +0900

    correct folder name

--- set_env.sh
+++ set_env.sh
@@ -3,6 +3,6 @@ cd ../
 git clone https://github.com/pjreddie/darknet
 cd darknet
 make
-cd ../headcounter
+cd ../py_cv2_yolo_obj_tracker
 cp ../darknet/data/coco.names .
 cp ../darknet/cfg/yolov3.cfg .
